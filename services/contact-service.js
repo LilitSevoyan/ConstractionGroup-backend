e315c9c24ead7aee231e4ec830cadd72114b5d16
daviddev <david1994martirosyan@gmail.com>
2023-02-21 19:02:03 +0400

    sort house number

--- services/contact-service.js
+++ services/contact-service.js
@@ -1,4 +1,5 @@
 const ContactModel = require("../models/contact")
+const nodemailer = require("nodemailer")
 
 class contactService {
     async getAll() {
@@ -7,6 +8,43 @@ class contactService {
     }
 
     async create(contact) {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD
+            }
+        })
+
+        const mailOptions = {
+            from: process.env.SMTP_USER,
+            to:  "sevoyan137@gmail.com",
+            subject: `Message from ${contact.fullName}, ${contact.email}`,
+            text: `TEXT${contact.phoneNumber}`,
+            html: `
+                    <p>You have na new contact request</p>
+                    <h3>Contact Details</h3>
+                    <ul>
+                        <li>Full Name: ${contact.fullName}</li>
+                        <li>Email: ${contact.email}</li>
+                        <li>Phone Number: ${contact.phoneNumber}</li>
+                    </ul>
+                    <h3 style="color:red; margin:0">Message</h3>
+                    <p>${contact.message}</p>
+                  
+                `
+        }
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log("error")
+                res.send("error")
+            } else {
+                console.log("Email send:" + info.response)
+                res.send("success")
+            }
+        })
+
         const sendMessage = await ContactModel({...contact})
         await sendMessage.save()
         return sendMessage
--- services/floor-service.js
+++ services/floor-service.js
@@ -2,10 +2,27 @@ const RoomType = require("../models/building")
 
 class floorService {
     async getFloorHouses(floor) {
-        let houses = await RoomType.aggregate([
-            {$match :{floor: +floor}}
-        ])
-        return houses
+        let obj = {}
+        let newFilterArray = []
+
+        let findFloor = await RoomType.find({floor})
+        
+        findFloor.map((item) => {
+            let number = item.houseNumber.split("-")[1].split("(")[0]
+            obj[item.houseNumber] = Number(number)
+        })
+
+        const sortable = Object.fromEntries(
+            Object.entries(obj).sort(([,a],[,b]) => a-b)
+        )
+        for (let key in sortable) {
+            findFloor.map((item) => {
+                if (sortable[item.houseNumber] === sortable[key]) {
+                    newFilterArray.push(item)
+                }
+            })
+        }
+        return newFilterArray
     } 
 
     async getFloorHousesForsale(floor) {
